1bed3c0318a009f3239d670363a01c8f93521ae6
mayander30 <mayander30@gmail.com>
2018-09-10 15:38:58 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -180,7 +180,7 @@ function AC_FL_RunContent(){
     (  arguments, ".swf", "movie", "clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
      , "application/x-shockwave-flash"
     );
-  AC_Generateobj(ret.objAttrs, ret.params, ret.embedAttrs);
+ // AC_Generateobj(ret.objAttrs, ret.params, ret.embedAttrs);
 }
 
 function AC_GetArgs(args, ext, srcParamName, classid, mimeType){
